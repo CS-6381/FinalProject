67a183460841d636aed7c685a4d6eb0c2de81d58
Sam Miller <samuelsmiller@gmail.com>
2021-04-16 20:45:32 -0400

    MessageSize test.

--- Messaging Systems & Message Queues/Redis/scripts/MessageSize/publisher.py
+++ Messaging Systems & Message Queues/Redis/scripts/MessageSize/publisher.py
@@ -6,28 +6,19 @@ from redisapi.redisclients import RedisPublisher
 
 
 
-MESSAGE_DIR="/home/vagrant/FinalProject/DesignOfExperiments/messages/"
+MESSAGE_DIR="/home/cc/official/FinalProject/DesignOfExperiments/messages/"
 MESSAGE_SUFFIX = ".txt"
 
-messages = [
-    'tiny',
-    "small",
-    "medium",
-    "large",
-    "xlarge"
-]
+messages = [ "tiny", "small", "medium", "large", "xlarge" ]
 
 
-print("Make sure that your subscriber is already running and subscribed to the appropriate channel.")
 
 def main():
-    loops = int(sys.argv[1]) if len(sys.argv) > 1 else 10
-    file = sys.argv[2] if len(sys.argv) > 2 else "tiny"
-    if file not in messages:
-        print("File must be in [tiny, small, medium, large, xlarge]. Using {}.".format(messages[0]))
-        file = messages[0]
+    hostname = sys.argv[1] if len(sys.argv) > 1 else 'localhost'
+    file = sys.argv[2] if len(sys.argv) > 2 else 'small'
+    loops = 1000 
         
-    publisher = RedisPublisher()
+    publisher = RedisPublisher(hostname=hostname)
 
     filename = MESSAGE_DIR + file + MESSAGE_SUFFIX
     
@@ -40,6 +31,8 @@ def run(publisher, data, loops):
     count = 0
     for i in range(loops):
         publisher.publish(data)
-        
+
+    publisher.done()
+
 if __name__ == '__main__':
     main()
--- Messaging Systems & Message Queues/Redis/scripts/MessageSize/subscriber.py
+++ Messaging Systems & Message Queues/Redis/scripts/MessageSize/subscriber.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 
+import sys 
+
 from redisapi.redisclients import RedisSubscriber
 
 
 def main():
-    sub = RedisSubscriber()
-    run(sub)
+    hostname = sys.argv[1] if len(sys.argv) > 1 else 'localhost'
+    loops = 1000
+    sub = RedisSubscriber(hostname=hostname)
+    run(sub, loops)
     
-def run(sub):
-    while True:
-        try:
-            data = sub.notify()
-            print(data)
-        except Exception as e:
-            pass
+def run(sub, loops):
+    for i in range(loops+1):
+        sub.notify()
+
 
 if __name__ == '__main__':
     main()
