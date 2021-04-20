d20d9ef215c8ac6dbc81dc2384e5b646ac9a5270
jmbeach <jaredbeachdesign@gmail.com>
2021-04-19 22:05:19 -0500

    data-processor - collect results

--- DesignOfExperiments/mq-data-processor/producer_file_parser.py
+++ DesignOfExperiments/mq-data-processor/producer_file_parser.py
@@ -4,6 +4,49 @@ from typing import List, Tuple
 
 
 class ProducerFileParser:
+    def __init__(self):
+        self.send_times = []
+        self.receive_times = []
+
+    @staticmethod
+    def _merge_sorted(a, b):
+        # combine sorted lists https://www.geeksforgeeks.org/python-combining-two-sorted-lists/
+        i = 0
+        j = 0
+        result = []
+        while i < len(a) and j < len(b):
+            if a[i] < b[j]:
+                result.append(a[i])
+                i += 1
+            else:
+                result.append(b[j])
+                j += 1
+
+        # add the remaining items
+        result = result + a[i:] + b[j:]
+        return result
+
+    def _collect_send(self, send_times):
+        if len(self.send_times) < 1:
+            self.send_times = send_times
+            return
+        self.send_times = ProducerFileParser._merge_sorted(send_times, self.send_times)
+
+    def _collect_receive(self, receive_times):
+        if len(self.receive_times) < 1:
+            self.receive_times = receive_times
+            return
+        self.receive_times = ProducerFileParser._merge_sorted(receive_times, self.receive_times)
+
+    def collect(self, send_receive_tuple):
+        """
+        Collects all of the data from a send/receive tuple (from parse_file_contents) into
+        the send_times and receive_times fields in sorted order
+        :param send_receive_tuple: Tuple[List[datetime], List[datetime]]
+        """
+        self._collect_send(send_receive_tuple[0])
+        self._collect_receive(send_receive_tuple[1])
+
     @staticmethod
     def parse_file_contents(file_contents) -> Tuple[List[datetime], List[datetime]]:
         """
