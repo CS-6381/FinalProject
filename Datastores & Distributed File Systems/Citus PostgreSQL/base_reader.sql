4100724a9ed7669038cbf342191636d0759a63eb
edmasters <76230161+edmasters@users.noreply.github.com>
2021-04-21 21:37:04 -0700

    reformatted reader

--- Datastores & Distributed File Systems/Citus PostgreSQL/base_reader.sql
+++ Datastores & Distributed File Systems/Citus PostgreSQL/base_reader.sql
@@ -3,13 +3,13 @@ $do$
 DECLARE
 	i INT;
 BEGIN
-	FOR i in 8001..10000
+	FOR i in 1..10000
 		LOOP
 			PERFORM *
-			FROM reader10
+			FROM data
 			ORDER BY id asc
 			LIMIT 1 OFFSET i;
-			INSERT INTO w25_r25 (reader_id, end_time) VALUES (5, clock_timestamp());
+			INSERT INTO data (reader_id, end_time) VALUES (1, clock_timestamp());
 		end LOOP;
 END;
 $do$;
