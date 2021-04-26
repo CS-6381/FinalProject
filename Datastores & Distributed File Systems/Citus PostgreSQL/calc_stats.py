4061c0c7443b1f44897beb6cfa455490f8f39a00
roblee357 <roblee357@hotmail.com>
2021-04-26 18:51:12 -0400

    calculations

---
+++ Datastores & Distributed File Systems/Citus PostgreSQL/calc_stats.py
@@ -0,0 +1,108 @@
+import pandas as pd
+
+import os, csv, datetime, time, uuid, numpy
+from pathlib import Path
+import dateutil.parser
+# from datetime import datetime, timedelta
+
+# Function to calculate average
+def getAverage(data):
+    total = sum(tuple(data))
+    average = total / len(data)
+    return average 
+
+# Function to calculate standard deviation
+def getStandardDeviation(data):
+    mean = sum(data) / len(data)
+    variance = sum([((x - mean) ** 2) for x in data]) / len(data)
+    res = variance ** 0.5
+    return res
+
+def mktimestamp(timestr):
+    supertime = dateutil.parser.parse(timestr)
+    theTS = supertime.timestamp()
+    # time.mktime(datetime.datetime.strptime(timestr,"%YYYY-%mm-%dd %H:%M:%S").timetuple())
+    return theTS
+
+def listDiff(listy):            # [1, 2, 3]
+    list1 = []
+    list2 = []
+    for i in listy:
+        list1.append(i)
+        list2.append(i)
+    list1.append(0)     # [1, 2, 3, 0]
+    list2.insert(0,0 )  # [0, 1, 2, 3]
+    diffList = []
+    zip_object = zip(list1, list2)
+    for list1_i, list2_i in zip_object:
+        diffList.append(list1_i-list2_i)
+    diffList.pop()
+    diffList.pop(0)      #    [1, 1]
+    return diffList
+
+def writeOutList(listyMcList,fname):
+    with open(fname + '.txt','w') as fout:
+        for i in listyMcList:
+            fout.write(str(i) + '\n')
+
+def removeNeg(list):
+    # Remove Negative Elements in List
+    # Using list comprehension
+    return [ele for ele in list if ele > 0]
+    
+
+
+def saveCalculations(df):
+        # Calculate statistics.
+    start_times = list(map(mktimestamp,df['start_time'].tolist()))
+    writeOutList(start_times,'start_times')
+    end_times =  list(map(mktimestamp,df['end_time'].tolist()))
+    writeOutList(end_times,'end_times')
+    newWriteTimes = removeNeg(listDiff(start_times))
+    print('newWriteTimes',newWriteTimes[0],type(newWriteTimes[0]),len(newWriteTimes))
+    newWriteTimes.sort()
+    writeOutList(newWriteTimes,'sortedNewWriteTimes')
+    newReadTimes =  removeNeg(listDiff(end_times))
+    
+    writeLatencyMin = min(newWriteTimes)
+    writeLatencyMax = max(newWriteTimes)
+    writeLatencyAverage = getAverage(newWriteTimes)
+    writeLatencyStandardDeviation = getStandardDeviation(newWriteTimes)
+    readLatencyMin = min(newReadTimes)
+    readLatencyMax = max(newReadTimes)
+    readLatencyAverage = getAverage(newReadTimes)
+    readLatencyStandardDeviation = getStandardDeviation(newReadTimes)
+    writeThroughputMin = (max(start_times)-min(start_times))/len(start_times)
+    # writeThroughputMax = max(writeSuccessRates)
+    # writeThroughputAverage = getAverage(writeSuccessRates)
+    # writeThroughputStandardDeviation = getStandardDeviation(writeSuccessRates)
+    # readThroughputMin = min(readSuccessRates)
+    # readThroughputMax = max(readSuccessRates)
+    # readThroughputAverage = getAverage(readSuccessRates)
+    # readThroughputStandardDeviation = getStandardDeviation(readSuccessRates)
+
+    print('writeLatencyMin = min(newWriteTimes)', writeLatencyMin)
+    print('writeLatencyMax = max(newWriteTimes)', writeLatencyMax)
+    print('writeLatencyAverage = getAverage(newWriteTimes)',writeLatencyAverage)
+    print('writeLatencyStandardDeviation = getStandardDeviation(newWriteTimes)',writeLatencyStandardDeviation)
+    print('readLatencyMin = min(newReadTimes)',readLatencyMin)
+    print('readLatencyMax = max(newReadTimes)',readLatencyMax)
+    print('readLatencyAverage = getAverage(newReadTimes)',readLatencyAverage)
+    print('readLatencyStandardDeviation = getStandardDeviation(newReadTimes)',readLatencyStandardDeviation)
+    print('writeThroughputMin',writeThroughputMin)
+
+# Loop across all timeDifferences files before saving CSV.
+folder = 'Results'
+print(os.listdir(folder))
+for f in os.listdir(folder):        
+    f = os.path.join(folder, f)
+    if '25r.csv' in str(f):
+        df = pd.read_csv(f)
+
+        df['diff_time'] = pd.to_datetime(df['end_time']) - pd.to_datetime(df['start_time'])
+        saveCalculations(df)
+        print(f)
+        print(df.head(10))
+
+thistestlist = [0,.25,.5, 2 ]
+print(listDiff(thistestlist))
---
+++ Datastores & Distributed File Systems/Citus PostgreSQL/calculate_statistics.py
@@ -0,0 +1,138 @@
+# Ref FinalProject/Datastores & Distributed File Systems/Apache Cassandra/JamesVu_CraigTopham_ThomasBoudreau/Code/calculateStatistics.py 
+# Use this to calculate statistics based on timeDifferences files.
+
+# Imports
+import os, csv, datetime, time, uuid, numpy
+from pathlib import Path
+from datetime import datetime, timedelta
+
+# Lists to store write or read time differences
+writeTimes = []
+readTimes = []
+
+# Lists to store rate of successful write or read iterations (e. g. '9/10' = 0.9)
+writeSuccessRates = []
+readSuccessRates = []
+
+# Establish directories.
+directory = Path('./Results') # ./timeDifferences/all
+                                                                                                         
+# Function to list all files in a directory                                                                                                                      
+def listFiles(directory):                                                                                                  
+    r = []                                                                                                            
+    subdirs = [x[0] for x in os.walk(directory)]                                                                            
+    for subdir in subdirs:                                                                                            
+        files = os.walk(subdir).__next__()[2]                                                                             
+        if (len(files) > 0):                                                                                          
+            for file in files:                                                                                        
+                r.append(os.path.join(subdir, file))                                                                         
+    return r
+
+# Function to convert list of times to proper format
+def convertTimes(data):
+    arr = []
+    for datum in data:
+        timeVal = float((datetime.strptime(datum, '%H:%M:%S.%f') - datetime.strptime('00', '%H')).total_seconds()*1000)
+        arr.append(timeVal)
+    return arr
+
+# Function to read a CSV file
+def readCSV(csvFile):
+    
+    # initializing the titles and rows list
+    fields = []
+    
+    # reading csv file
+    with open(csvFile, 'r') as cf:
+        # creating a csv reader object
+        csvReader = csv.reader(cf)
+        
+        # extracting field names through first row
+        fields = next(csvReader)
+    
+        # extracting each data row one by one
+        successCount = 0
+        fileName = str(csvFile)
+        for row in csvReader:
+            if '_w_' in fileName:
+                writeTimes.append(row[5])
+            if '_r_' in fileName:
+                readTimes.append(row[5])
+            successCount = successCount + 1
+        successMax = ''
+        if '_w_' in fileName:
+            writeSuccessRates.append(successCount/int(fileName.split('_w_')[1].split('_')[0]))
+        if '_r_' in fileName:
+            readSuccessRates.append(successCount/int(fileName.split('_r_')[1].split('_')[0]))
+    return 'done'
+
+# Function to calculate average
+def getAverage(data):
+    total = sum(tuple(data))
+    average = total / len(data)
+    return average 
+
+# Function to calculate standard deviation
+def getStandardDeviation(data):
+    mean = sum(data) / len(data)
+    variance = sum([((x - mean) ** 2) for x in data]) / len(data)
+    res = variance ** 0.5
+    return res
+
+# Function to save calculations to a CSV file
+def saveCalculations():
+
+    # Calculate statistics.
+    newWriteTimes = convertTimes(writeTimes)
+    newReadTimes = convertTimes(readTimes)
+    writeLatencyMin = min(newWriteTimes)
+    writeLatencyMax = max(newWriteTimes)
+    writeLatencyAverage = getAverage(newWriteTimes)
+    writeLatencyStandardDeviation = getStandardDeviation(newWriteTimes)
+    readLatencyMin = min(newReadTimes)
+    readLatencyMax = max(newReadTimes)
+    readLatencyAverage = getAverage(newReadTimes)
+    readLatencyStandardDeviation = getStandardDeviation(newReadTimes)
+    writeThroughputMin = min(writeSuccessRates)
+    writeThroughputMax = max(writeSuccessRates)
+    writeThroughputAverage = getAverage(writeSuccessRates)
+    writeThroughputStandardDeviation = getStandardDeviation(writeSuccessRates)
+    readThroughputMin = min(readSuccessRates)
+    readThroughputMax = max(readSuccessRates)
+    readThroughputAverage = getAverage(readSuccessRates)
+    readThroughputStandardDeviation = getStandardDeviation(readSuccessRates)
+
+    # Save CSV. Create the folder first.
+    saveFile = "./calculations/Calculations_" + datetime.now().strftime("%m-%d-%Y_%H%M%S") + "_" + str(uuid.uuid4()) + ".csv"
+    with open(saveFile, "w") as csvFile:
+        csvWriter = csv.writer(csvFile)
+        csvWriter.writerow(['Metric', 'Value'])
+        csvWriter.writerow(['writeLatencyMin', writeLatencyMin])
+        csvWriter.writerow(['writeLatencyMax', writeLatencyMax])
+        csvWriter.writerow(['writeLatencyAverage', writeLatencyAverage])
+        csvWriter.writerow(['writeLatencyStandardDeviation', writeLatencyStandardDeviation])
+        csvWriter.writerow(['readLatencyMin', readLatencyMin])
+        csvWriter.writerow(['readLatencyMax', readLatencyMax])
+        csvWriter.writerow(['readLatencyAverage', readLatencyAverage])
+        csvWriter.writerow(['readLatencyStandardDeviation', readLatencyStandardDeviation])
+        csvWriter.writerow(['writeThroughputMin', writeThroughputMin])
+        csvWriter.writerow(['writeThroughputMax', writeThroughputMax])
+        csvWriter.writerow(['writeThroughputAverage', writeThroughputAverage])
+        csvWriter.writerow(['writeThroughputStandardDeviation', writeThroughputStandardDeviation])
+        csvWriter.writerow(['readThroughputMin', readThroughputMin])
+        csvWriter.writerow(['readThroughputMax', readThroughputMax])
+        csvWriter.writerow(['readThroughputAverage', readThroughputAverage])
+        csvWriter.writerow(['readThroughputStandardDeviation', readThroughputStandardDeviation])
+    return saveFile
+
+# Loop across all timeDifferences files before saving CSV.
+folder = 'Results'
+print(os.listdir(folder))
+for f in os.listdir(folder):        
+    f = os.path.join(folder, f)
+    if '.csv' in str(f):
+        print(f)
+        readCSV(f)
+    
+saveFile = saveCalculations()
+print("Result at: " + saveFile)
